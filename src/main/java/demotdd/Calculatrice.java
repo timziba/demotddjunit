f03f2833e3e2e5b6563aa7aa21d129e402d64ff9
timziba <timziiiba95@gmail.com>
2021-02-28 21:11:12 +0100

    ajout test unitaire

---
+++ src/main/java/demotdd/Calculatrice.java
@@ -0,0 +1,15 @@
+package demotdd;
+
+public class Calculatrice {
+
+	public int addition(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+	public int soustraction(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+
+}
--- src/test/java/demotdd/CalculatriceServiceTest.java
+++ src/test/java/demotdd/CalculatriceServiceTest.java
@@ -1,84 +1,128 @@
 package demotdd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
+import org.junit.Assume;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
+
+@DisplayName("class de test calculatrice")
 public class CalculatriceServiceTest {
 
-	private CalculatriceService calculatrice;
+	Calculatrice calculatrice = new Calculatrice();
 	
-/*
-	@Before
-	public void setUp() { //SETUP
-		calculatrice = new CalculatriceService();
-		System.out.println("setUp");
-		}
-	
-	@Test(timeout = 20)
-	public void testAddition() {
-		//fail("not implemented");
-		int resultat = calculatrice.addition(5,6); //EXCERCISE
-		assertEquals(resultat,11);
-	}
-	
-	@Ignore
-	@Test(expected = ArithmeticException.class)
-	public void testDivision() {
-		//fail("not implemented");
-		calculatrice.division(10,0);
-		System.out.println("division Fait");
+	@Test
+	@DisplayName("methode d'addition")
+	public void testAdditionEntier() {
 		
-	}
+		int resultatAddition = calculatrice.addition(2,3);
+		//fail();
+		assertEquals(5, resultatAddition);
+	}	
 	
-	
-	
-	@After
-	public void tearDown() {
-		System.out.println("tesrDown");
-		calculatrice= null;
-	}
-	*/
+	///Methode de test qui sera executer avnat tout test
 	
 	@BeforeEach
-	public void setUp() { //SETUP
-		calculatrice = new CalculatriceService();
-		System.out.println("setUp");
+	@Test
+	public void testSoustractionEntier() {
+		Calculatrice calculatrice = new Calculatrice();
+		int resultatAddition = calculatrice.soustraction(3,1);
+		//fail();
+		assertEquals(2, resultatAddition);
+	}
+	
+	///Methode qui s'execute avant tout test
+	
+	@Test
+	@BeforeEach
+	public void sayHello() {
+		System.out.println("hello");
+	}
+	
+	///Methode qui s'execute apres tout test
+	
+		@Test
+		@AfterEach
+		public void sayBye() {
+			System.out.println("Bye ! See you next");
 		}
-	
-	@Test
-	public void testAddition() {
-		//fail("not implemented");
-		int resultat = calculatrice.addition(6, 5);
-		Assertions.assertTimeout(Duration.ofMillis(10), ()->assertEquals(11, resultat));
 		
-	}
-	
-	
-	@Disabled
-	@Test
-	public void testDivision() {
-		//fail("not implemented");
-		Assertions.assertThrows(ArithmeticException.class,()->calculatrice.division(10,0));
-		System.out.println("division Fait");
+		// Cette methode de test s'execute que sur linux
 		
-	}
-	
-	
-	@AfterEach
-	public void tearDown() {
-		System.out.println("tesrDown");
-		calculatrice= null;
-	}
+		
+		/*
+		 * @Test
+		 * 
+		 * @DisabledOnOs(OS.WINDOWS) public void testEnableOnOs() { assertEquals(3,
+		 * calculatrice.soustraction(5, 2)); }
+		 */
+		
+		@Test
+		@EnabledIf("true")
+		public void testEnableCondition() {
+			assertTrue(true);
+			System.out.println("Condition ok");
+		}
+		
+		
+		
+		@Test
+		@Disabled("Ce test est désactivé")
+		public void testDisabled() {
+			assertEquals(5, calculatrice.soustraction(10, 5));
+		}
+
+		// Test  qui va s'executer en 1 position apres tout test avec l'annotation @BeforeAll 
+
+		@Test
+		@Order(1)
+		public void testOrdered() {
+			int resultat = calculatrice.addition(5, 5);
+			assertEquals(10, resultat);
+		}
+		
+		// Test  qui va s'executer en 2 position apres tout test avec l'annotation @BeforeAll 
+		
+		@Test
+		@Order(2)
+		public void testOrderedSecond() {
+			int resultat = calculatrice.addition(6, 5);
+			assertEquals(11, resultat);
+		}
+		
+		@Test
+		@RepeatedTest(2)
+		public void testRepeated() {
+			assertEquals(4, calculatrice.addition(2, 2));
+		}
+		
+		@Nested
+		class subClassTest{
+			
+			@Test
+			public void firstTestInSubClass() {
+				assertEquals(3, calculatrice.soustraction(6, 3));
+			}
+		}
+		
+
 }
